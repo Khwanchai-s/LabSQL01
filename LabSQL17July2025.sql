e212d5ec3d4d40975eb48f3100127dde3b361ea3
Khwanchai-s <khwanchai.s@gmail.com>
2025-07-17 15:38:43 +0700

    fffff

--- LabSQL17July2025.sql
+++ LabSQL17July2025.sql
@@ -1,2 +1,6 @@
-Select * from Products
+--ต้องการข้อมูลพนักงานทั้งหมด
 Select * from Employees
+--รหัสพนักงานชื่อนามสกุลพนักงานทุกคน
+Select EmployeeID,FirstName,LastName from Employees
+--พนักงานที่อยู่เมือง london
+Select * from Employees where City = 'london'
