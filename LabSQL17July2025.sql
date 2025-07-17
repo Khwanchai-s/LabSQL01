e2c9034d75fe0f616a8e35877e4179aa66f9e7f2
Khwanchai-s <khwanchai.s@gmail.com>
2025-07-17 17:09:57 +0700

    ffff

--- LabSQL17July2025.sql
+++ LabSQL17July2025.sql
@@ -20,6 +20,25 @@ where CompanyName like 'A%'
 --ต้องการข้อมูลลูกค้าที่มีชื่อลงท้ายด้วย Y
 Select * from Customers
 where CompanyName like '%Y'
+--ต้องการชื่อ นามสกุล พนักงานที่มีชื่อประกอบด้วยตัวอักษร 'an'
+select firstname, lastname from Employees
+where FirstName LIKE '%an%'
+--ต้องการชื่อบริษัทลูกค้าที่มีตัวอักษรตัวที่ 2 เป็น 'A'
+Select CompanyName from customers
+where CompanyName like '_A%'
+-- ต้องการชื่อ ราคาสินค้า,จำนวนคงเหลือ ที่มีจำนวนคงเหลือสูงที่สุด 10 อันดับแรก
+Select top 10 ProductName,UnitPrice , UnitsInStock
+from Products
+order by UnitsInStock desc
+--แสดงข้อมูลรหัสประเภทสินค้า, ชื่อสินค้า และราคา 
+--โดยเรียงลำดับตามรหัสประเภทสินค้าจากน้อยไปมาก 
+--หากรหัสประเภทเป็นรหัสเดียวกันให้เรียงตามราคาสินค้าจากมากไปน้อย
+
+SELECT CategoryID, ProductName, UnitPrice
+FROM Products
+ORDER BY CategoryID ASC , UnitPrice DESC
+
+
 
 
 
